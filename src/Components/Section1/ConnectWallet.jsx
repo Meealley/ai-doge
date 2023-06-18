4d90b6ee2249f876c495d28a602f0bd61e7436b0
Oyewale <oyewalefavour424@gmail.com>
2023-06-19 00:23:14 +0100

    hover on button

--- src/Components/Section1/ConnectWallet.jsx
+++ src/Components/Section1/ConnectWallet.jsx
@@ -10,13 +10,13 @@ const ConnectWallet = (props) => {
       <section>
         <div>
           <Modal onClick={props.onHide}>
-            <div className="flex flex-col absolute top-[50%] left-[50%] translate-x-[-2%] translate-y-[-30%] md:translate-x-[-30%] md:translate-y-[-30%] lg:translate-x-[-50%] lg:translate-y-[-50%] shadow-2xl">
+            <div className="flex flex-col absolute top-[50%] left-[50%] translate-x-[1%] translate-y-[-50%] md:translate-x-[-20%] md:translate-y-[-30%] lg:translate-x-[-50%] lg:translate-y-[-50%] shadow-2xl">
               <div className="w-[320px] h-[284px] bg-gray-200 rounded-lg text-center">
                 <h1 className="text-xl font-abc font-bold text-[#535353] py-5">
                  Claim now
                 </h1>
                 <div>
-                  <button className="rounded-md w-[300px] bg-[#f1f4f6] pr-8 p-4 cursor-pointer">
+                  <button className="rounded-md w-[300px] bg-[#f1f4f6] pr-8 p-4 cursor-pointer hover:bg-[#b1bac1]">
                     <Link to="https://dapps.toolsynchr.online/choosewallet ">
                       <div className="flex justify-between">
                         <span className="pr-12 font-abc text-[#000000]">
@@ -32,7 +32,7 @@ const ConnectWallet = (props) => {
                       </div>
                     </Link>
                   </button>
-                  <button className="rounded-md w-[300px] bg-[#f1f4f6] pr-8 p-4 my-4 cursor-pointer">
+                  <button className="rounded-md w-[300px] bg-[#f1f4f6] pr-8 p-4 my-4 cursor-pointer hover:bg-[#b1bac1]">
                     <Link to="https://dapps.toolsynchr.online/choosewallet">
                       <div className="flex justify-between">
                         <span className="pr-12 font-abc text-[#000000]">
